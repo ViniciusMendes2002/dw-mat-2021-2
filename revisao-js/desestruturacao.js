869a1731c48cd18ad7491d88e49254969c5b394a
ViniciusMendes <viniciusmferreira70@gmail.com>
2021-07-30 17:39:33 +0000

    (30/07) Início da revisão de JS

--- revisao-js/desestruturacao.js
+++ revisao-js/desestruturacao.js
@@ -1,18 +1,11 @@
 // Vetor
 let numeros = [10, 20, 30]
 
-// Objeto
-let carro = {
-    modelo: 'Fusca',
-    marca: 'Volkswagen',
-    cor: 'Preto',
-    ano: 1969
-}
-
 // Desestruturação do vetor
 let [a, b, c] = numeros
 
-console.log(a,b, c)
+console.log(a, b, c)
+console.log('a: ' + a)
 
 // Desestruturando apenas o segundo valor do vetor
 let [, x, ] = numeros
@@ -22,12 +15,18 @@ console.log('x: ' + x)
 let [i, j] = numeros
 console.log(`i: ${i}, j: ${j}`)
 
-// Na desestruturação do objeto, as variáveis precisam ter, NECESSARIAMENTE,
+// Objeto
+let carro = {
+  modelo: 'Fusca',
+  marca: 'Volkswagen',
+  cor: 'Preto',
+  ano: 1969
+}
+
+// Na desestruturação de objetos, as variáveis precisam ter, NECESSARIAMENTE,
 // o mesmo nome das propriedades, MAS A ORDEM NÃO IMPORTA
-
-let {marca, modelo,ano, cor } = carro
-
+let { marca, modelo, ano, cor } = carro
 console.log(`marca: ${marca}, modelo: ${modelo}, ano: ${ano}, cor: ${cor}`)
 
 // Desestruturação parcial
-// 
+// let { marca, modelo } = carros
